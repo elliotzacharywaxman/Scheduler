472b44fb9e56a337b6d3165c5127e4ce4fdfbf6d
elliotzacharywaxman <elliot@climatedao.xyz>
2022-05-27 19:05:38 -0400

    new commit

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-// var currentDay = document.getElementById("currentDay")
-// var currentDay = $("#currentDay")
-// currentDay.text(getDay(time))
-
-// function getDay(time) {
-//     time = moment().format("MMM Do YY"); 
-//     return time;
-// }
-
-// setInterval(getDay,1000)
-
 function setTime(){
     var today = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(today);
 }    
-
 setInterval(setTime, 1000);
+
+hours
