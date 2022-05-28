2a4ae7c964ee8353f89d8d7f319254a70a2d32eb
elliotzacharywaxman <elliot@climatedao.xyz>
2022-05-27 20:32:12 -0400

    adding functionality to event tracker

--- script.js
+++ script.js
@@ -4,4 +4,41 @@ function setTime(){
 }    
 setInterval(setTime, 1000);
 
-hours
+{/* <tr>
+<td>9:00am</td>
+<td><textarea></textarea></td>
+<td><button class="saveBtn fa fa-folder"></button></td>
+</tr> */}
+
+var tbodyEl = document.getElementsByClassName("bodyOn")
+console.log(tbodyEl)
+
+
+var hours = 18
+
+
+
+var renderRows = () => {
+    $("bodyOn").html("");
+    for (i = 10; i < hours; i++){
+        var tableRowContainer = $("<tr>");
+        var hourData = $("<td>")
+        var eventData = $("<td>")
+        var event = $("<textarea>")
+        var btnSq = $("<td>")
+        var saveBtn = $("<button>")
+        
+        hourData.text(i)
+        hourData.appendTo(tableRowContainer);
+        eventData.appendTo(tableRowContainer);
+        event.appendTo(eventData);
+        btnSq.appendTo(tableRowContainer);
+        saveBtn.appendTo(btnSq);
+
+        
+
+        tableRowContainer.appendTo(tbodyEl);
+    }
+};
+
+renderRows()
